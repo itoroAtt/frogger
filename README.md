681266693d04e7fc475e69a9f317d168c1c963a1
itoroATT <ia365u@att.com>
2015-01-30 17:24:51 -0500

    Final version of frogger game - basic edition

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-frontend-nanodegree-arcade-game
+Itoro Akpan-Iquot's frontend-nanodegree-arcade-game - Frogger
 ===============================
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+Instructions for playing the Frogger game:
 
-for self-checking their submission.
+1) The aim of the gam is to move the player piece from the bottom of the board to the water (top of board), 
+without colliding with the Enemy bugs
+
+2) If player collides with bug, its position is reset to the bottom of the column where the collision occurred
+
+3) Player can move up, down, left or right, using the appropriate arrow keys
+
+4) There is neither a time limit nor a "life limit"
+
+4) The version of the Frogger game is bare bones.  No special effects or moves were added
+
+5) Have fun!  Good luck!!
--- js/app.js
+++ js/app.js
@@ -1,113 +1,126 @@
-// Enemies our player must avoid
+/**
+ * Creates Enemy Player Piece
+ * @constructor
+ */
 var Enemy = function(x,y) {
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
-
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
 	this.sprite = 'images/enemy-bug.png';
 	this.x = x;
 	this.y = y;
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-		this.x = this.x + (dt * 50);
-		if(this.x > 505) {
-			this.x = 0;
-		}
-}
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/**
+ * Creates Player Piece
+ * @constructor
+ */
 var Player = function() {
 	this.sprite = 'images/char-pink-girl.png';
 	this.x = 0;
 	this.y = 400;
 }
 
-Player.prototype.update = function() {
-	var collision = this.checkPlayerCollisions();
-	if(this.y || collision) {
-		this.reset();
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [], defaultEnemyLanesX = [-200, -100, 50, 100, 200], defaultEnemyLanesY = [65, 145, 225];
+var enemyCounter;
+var enemy1, enemy2, enemy3, enemy4, enemy5;
+var player;
+
+//Create 5 Enemy pieces
+enemy1 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
+enemy2 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
+enemy3 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
+enemy4 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
+enemy5 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
+allEnemies.push(enemy1,enemy2,enemy3,enemy4,enemy5);
+
+player = new Player();
+
+
+/**
+ * Update Enemy position, required method for game
+ * @param {number} dt is a time delta between ticks.
+ */
+Enemy.prototype.update = function(dt) {
+	this.x = this.x + (dt * 50);
+	if(this.x > 505) {
+		this.x = 0;
 	}
+}
+
+/**
+ * Draw the enemy on the screen, required method for game
+ */
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+/**
+ * Update Player position, required method for game
+ */
+Player.prototype.update = function() {
+	this.checkPlayerCollisions();
+	
 	
 }
 
-// Draw the enemy on the screen, required method for game
+/**
+ * Draw the Player on the screen, required method for game
+ */
 Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+/**
+ * Checks to see if Enemy and Player are in the same X,Y Plane.  If so, Player piece is reset to starting position.
+ */
 Player.prototype.checkPlayerCollisions = function() {
 	var counter;
-	
-		// write some code that checks if enemy.xPosition >= player.xPosition
-		// if true, then set playerCollision = true
-		// I hope you know what I mean by "xPosition"; maybe you called that variable something else.
-		for (counter in allEnemies) {
-			if (player.x < allEnemies[counter].x) {
-				return true;
-			}
+	if(this.y <60) {
+		this.reset();
+	}
+	for (counter in allEnemies) {
+		if(allEnemies[counter].x+70 > this.x+10 && allEnemies[counter].x <this.x+85 && allEnemies[counter].y+5 >= this.y) {
+			player.reset();		
 		}
-		return false;
-
+	}
 }
 
+/**
+ * Resets Player piece to bottom of column where collision occured.
+ */
 Player.prototype.reset = function() {
 	this.y = 400;
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [], defaultEnemyLanesX = [205, 0, 300, 400, 122], defaultEnemyLanesY = [65, 145, 225];
-var enemyCounter;
-var enemies;
-var enemy1, enemy2, enemy3, enemy4, enemy5;
-var player;
-
-//for(enemyCounter = 0; enemyCounter < defaultEnemyLanes.length; enemyCounter++) {
-	enemy1 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
-	enemy2 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
-	enemy3 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
-	enemy4 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
-	enemy5 = new Enemy(defaultEnemyLanesX[Math.floor(Math.random() * 5)],defaultEnemyLanesY[Math.floor(Math.random() * 3)]);
-	allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
-//}
-
-player = new Player();
-
+/**
+ * Moves Player piece in the direction of arrow key pressed
+ * @param {keyPressed} keyPressed is the keyboard key that was pressed.
+ */
 Player.prototype.handleInput = function(keyPressed) {
-  if(keyPressed === 'right' && this.x < 400) {
-    this.x += 100;
-  }
+	if(keyPressed === 'right' && this.x < 400) {
+		this.x += 100;
+	}
 
-  if(keyPressed ==='left' && this.x > 82) {
-    this.x -= 100;
-  }
+	if(keyPressed ==='left' && this.x > 82) {
+		this.x -= 100;
+	}
 
-  if(keyPressed === 'down' && this.y < 400) {
-    this.y += 85;
-  }
+	if(keyPressed === 'down' && this.y < 400) {
+		this.y += 85;
+	}
 
-  if(keyPressed === 'up' && this.y > 52) {
-    this.y -= 85;
-  }
+	if(keyPressed === 'up' && this.y > 52) {
+		this.y -= 85;
+	}
 }
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+ * Listens for key presses and sends the keys to the Player.handleInput() method
+ * @param {e} event is what we are listening for, in this case, key pressed
+ */
+// This 
+// . You don't need to modify this.
 document.addEventListener('keyup', function(e) {
 	var allowedKeys = {
 		37: 'left',
